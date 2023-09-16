65a295fc1dafc5e88ae93b415aee23757b586381
zivyuan <ziv.yuan@gmail.com>
2023-09-16 09:58:02 +0800

    fix missing erplacement

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,6 +10,7 @@ sed  -i '' "s/uni-common-plugin-preset/${PROJ}/g" src/pages/index/index.vue
 mv src/uni_modules/uni-common-plugin-preset/components/uni-common-plugin-preset/uni-common-plugin-preset.vue src/uni_modules/uni-common-plugin-preset/components/uni-common-plugin-preset/$PROJ.vue
 mv src/uni_modules/uni-common-plugin-preset/components/uni-common-plugin-preset src/uni_modules/uni-common-plugin-preset/components/$PROJ
 mv src/uni_modules/uni-common-plugin-preset src/uni_modules/$PROJ
+sed  -i '' "s/uni-common-plugin-preset/${PROJ}/g" src/uni_modules/$PROJ/components/$PROJ/$PROJ.vue
 sed  -i '' "s/uni-common-plugin-preset/${PROJ}/g" src/uni_modules/$PROJ/readme.md
 sed  -i '' "s/uni-common-plugin-preset/${PROJ}/g" src/uni_modules/$PROJ/package.json
 sed  -i '' "s/uni-common-plugin-preset/${PROJ}/g" src/pages.json
